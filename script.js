e055449dcc139074dc77538edd696ef05e0b2d56
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-06-25 10:42:48 +0100

    feat: improve navigation bar style

--- script.js
+++
@@ -1,8 +0,0 @@
-// Toggles the visibility of
-// the navigation menus
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
