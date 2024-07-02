cde58018fbbc1165269ef862c747320bebae1e53
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-07-02 17:26:52 +0100

    feat: store user's light/dark mode preference

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggleButton = document.getElementById('theme-toggle');
     const body = document.body;
 
+    const userPreference = localStorage.getItem('themePreference');
+    if (userPreference === 'light') {
+        body.classList.add('light-mode-enabled');
+        body.classList.add('light-mode');
+        themeToggleButton.setAttribute('fill', '#171616');
+    }
+
     themeToggleButton.addEventListener('click', () => {
         body.classList.toggle('light-mode-enabled');
 
-        // Check if light mode is enabled
         if (body.classList.contains('light-mode-enabled')) {
             body.classList.add('light-mode');
             themeToggleButton.setAttribute('fill', '#171616');
+            localStorage.setItem('themePreference', 'light');
         } else {
             body.classList.remove('light-mode');
             themeToggleButton.setAttribute('fill', '#e8eaed');
+            localStorage.setItem('themePreference', 'dark');
         }
     });
 });
